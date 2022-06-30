20788b8dcdc785e126986eedd48b847dbceb2985
Natalia Tomenko <nataliatomenko13@gmail.com>
2022-06-30 22:45:25 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,10 @@
 
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.text.*;
-import javax.swing.text.DefaultEditorKit.*;
-import javax.swing.text.StyledEditorKit.*;
-import javax.swing.plaf.nimbus.NimbusLookAndFeel;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.swing.border.LineBorder;
 import javax.swing.undo.UndoManager;
-import javax.swing.event.*;
-import java.util.Random;
-import java.io.*;
 
 public class Main {
 
-//    private UndoManager undoMgr;
-//    private String pictureButtonName;
-//    private File file;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         JFrame frame = new JFrame();
         JTextPane textPane = new JTextPane();
